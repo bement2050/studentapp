309e964fa86705e896a4a9afd49a182931237e67
bement2050 <bement2050@users.noreply.github.com>
2026-09-25 09:12:47 -0400

    Add color-coded emotion feedback

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "todays-journal-v43";
-const APP_FILES = ["./index.html", "./styles.css?v=43", "./app.js?v=43", "./manifest.json"];
+const CACHE_NAME = "todays-journal-v44";
+const APP_FILES = ["./index.html", "./styles.css?v=44", "./app.js?v=44", "./manifest.json"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES)));
